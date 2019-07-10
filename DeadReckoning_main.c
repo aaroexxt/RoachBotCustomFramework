30a1c31ea62e17b542f2a3b708e51c6456491a25
Aaron Becker <aaron.becker@gmail.com>
2019-07-10 15:26:08 -0700

    upd timers

--- DeadReckoning_main.c
+++ DeadReckoning_main.c
@@ -8,28 +8,17 @@
 //A library to use the uc32 board
 #include "BOARD.h"
 #include "roach.h"
+#include "timers.h"
 
 #define ONE_SECOND 500000
 
-/*
- * 
- */
+#define TIMER_A 0
+#define TIMER_B 1
 
-void waitOneSecond()
-{
-    int i = 0;
-        while (i < ONE_SECOND) {
-            i++;
-        }
+void delayMs(int ms){
+    
 }
 
-void delayMs(int ms)
-{
-    int i = 0;
-    while (i < (ONE_SECOND/1000)*ms) {
-        i++;
-    }
-}
 void driveForwardMs(int ms) {
     Roach_RightMtrSpeed(100);
     Roach_LeftMtrSpeed(100);
@@ -72,38 +61,91 @@ int main(void)
     //These calls configure the Pic32 so that it can interact with the Roach hardware
     BOARD_Init();
     Roach_Init();
+    TIMERS_Init();
+    
+    TIMERS_InitTimer(0, 50);
+    TIMERS_InitTimer(1, 100);
+    
 
     printf("Welcome to Dead Recokining!  This program was compiled on %s as %s.\r\n", __DATE__, __TIME__);
 
     //Initialization code here:
 
-    int i;
-
-
+    int ledState = 0;
+    
+    Roach_RightMtrSpeed(100);
+    Roach_LeftMtrSpeed(100);
     while (1) {
-        driveForwardMs(1200);
-        turnLeftMs(250);
-        driveForwardMs(2400);
-        turnLeftMs(500);
-        driveForwardMs(3400);
-        turnLeftMs(750);
-        driveForwardMs(1500);
-        turnLeftMs(350);
-        driveForwardMs(2100);
-        
-        turnRightMs(300);
-        driveForwardMs(750);
-        turnRightMs(700);
-        driveForwardMs(2500);
-        delayMs(5000);
-        while (1) {
+        if(TIMERS_IsTimerExpired(0)) {
+            //set clear, reset
+            TIMERS_ClearTimerExpired(0);
+            TIMERS_InitTimer(0, 50);
+            
+            switch(ledState) {
+                case 0:
+                    Roach_LEDSSet(0b000000000000);
+                    ledState++;
+                    break;
+                case 2<<12:
+                    ledState = 0;
+                    break;
+                default:
+                    Roach_LEDSSet(ledState);
+                    ledState++;
+                    break;
+            }
+            
+        }
+        if(TIMERS_IsTimerExpired(1)) {
+            //set clear, reset
+            TIMERS_ClearTimerExpired(1);
+            TIMERS_InitTimer(1, 100);
+            
             int value = Roach_LightLevel();
             value = map(value, 0, 1023, 0, 100);
             Roach_RightMtrSpeed(value);
             Roach_LeftMtrSpeed(value);
+            
         }
-        break;
-    };
+        
+        if (Roach_ReadFrontLeftBumper() || Roach_ReadFrontRightBumper()) {
+            TIMERS_StopTimer(1); //pause timer1
+            Roach_RightMtrSpeed(-100);
+            Roach_LeftMtrSpeed(-100);
+            int i = 0;
+            for(i=0; i<100000; i++){}
+            TIMERS_StartTimer(1);
+        }
+        //printf("%d",(int)TIMERS_GetTime());
+    }
+    
+    
+    
+    
+//    while (1) {
+//        driveForwardMs(1200);
+//        turnLeftMs(250);
+//        driveForwardMs(2400);
+//        turnLeftMs(500);
+//        driveForwardMs(3400);
+//        turnLeftMs(750);
+//        driveForwardMs(1500);
+//        turnLeftMs(350);
+//        driveForwardMs(2100);
+//        
+//        turnRightMs(300);
+//        driveForwardMs(750);
+//        turnRightMs(700);
+//        driveForwardMs(2500);
+//        delayMs(5000);
+//        while (1) {
+//            int value = Roach_LightLevel();
+//            value = map(value, 0, 1023, 0, 100);
+//            Roach_RightMtrSpeed(value);
+//            Roach_LeftMtrSpeed(value);
+//        }
+//        break;
+//    };
 
     return (EXIT_SUCCESS);
 }
